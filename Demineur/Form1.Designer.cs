a75ee33964c08280501f27c89bdcd1a06494411b
pj67hvs <pj67hvs@INF-N104-M103>
2022-06-10 15:07:08 +0200

    Ajoutez des fichiers projet.

---
+++ Demineur/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Demineur
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.lblFlag = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.lblNbrOfBombs = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.lblNbrOfBombs);
+            this.splitContainer.Panel1.Controls.Add(this.lblFlag);
+            this.splitContainer.Panel1.Controls.Add(this.lblScore);
+            this.splitContainer.Panel1.Controls.Add(this.btnNewGame);
+            this.splitContainer.Size = new System.Drawing.Size(942, 534);
+            this.splitContainer.SplitterDistance = 141;
+            this.splitContainer.TabIndex = 0;
+            // 
+            // lblFlag
+            // 
+            this.lblFlag.AutoSize = true;
+            this.lblFlag.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblFlag.Location = new System.Drawing.Point(28, 87);
+            this.lblFlag.Name = "lblFlag";
+            this.lblFlag.Size = new System.Drawing.Size(222, 15);
+            this.lblFlag.TabIndex = 2;
+            this.lblFlag.Text = "To flag a button use the mouse wheel !";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(28, 30);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(45, 15);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score : ";
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(455, 0);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(487, 138);
+            this.btnNewGame.TabIndex = 0;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // lblNbrOfBombs
+            // 
+            this.lblNbrOfBombs.AutoSize = true;
+            this.lblNbrOfBombs.Location = new System.Drawing.Point(201, 30);
+            this.lblNbrOfBombs.Name = "lblNbrOfBombs";
+            this.lblNbrOfBombs.Size = new System.Drawing.Size(114, 15);
+            this.lblNbrOfBombs.TabIndex = 3;
+            this.lblNbrOfBombs.Text = "Number of bombs : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 534);
+            this.Controls.Add(this.splitContainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Demineur";
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblFlag;
+        private System.Windows.Forms.Label lblNbrOfBombs;
+    }
+}
+
---
+++ Demineur/Form1.cs
@@ -0,0 +1,224 @@
+﻿///My version of minesweeper, when a non bomb button is clicked a text in the button will appear and tell how far the clicked button is from the nearest bomb
+
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Demineur
+{
+    public partial class Form1 : Form
+    {   
+        Bitmap flag = new Bitmap(@"F:\05-repertoires-ict-ssd\AUTRES\Demineur\Demineur\Images\flag.png");//Must be changed if used somewhere else
+        int nbrOfBomb;//number of bombs
+        bool quit = false;//Bool to know if it has to go out of the double for loop
+        int score;//Score
+        Random random = new Random();//Random
+        int randomNumber;//Result of the random (to know if the button is a bomb or not)
+        const int NBROWCOLS = 10;//Not touch (Number of cols and rows)
+        Button[,] arrButtons = new Button[NBROWCOLS, NBROWCOLS];//Array of all the buttons
+        public Form1()
+        {
+            InitializeComponent();
+            InitGame();
+        }
+
+        private void InitGame()
+        {
+            int btnWidth = splitContainer.Panel2.Size.Width / NBROWCOLS;
+            int btnHeight = splitContainer.Panel2.Size.Height / NBROWCOLS;
+            for (int i = 0; i < NBROWCOLS; i++)
+            {
+                for (int j = 0; j < NBROWCOLS; j++)
+                {
+                    randomNumber = random.Next(0, 8);
+                    Point coordBtn = new Point(j * btnWidth, i * btnHeight);//btnCoord
+                    arrButtons[i, j] = new Button();//Add the button to array
+                    arrButtons[i, j].Location = coordBtn;//Location of the button
+                    arrButtons[i, j].Size = new Size(btnWidth, btnHeight);//Size of the button
+                    arrButtons[i, j].BackColor = Color.LightGray;//Backcolor of the button
+
+                    splitContainer.Panel2.Controls.Add(arrButtons[i, j]);//Add the button to the split container
+
+                    //If the random is 1 the button is a bomb
+                    if (randomNumber == 1)
+                    {
+                        nbrOfBomb++;
+                        arrButtons[i, j].Tag = "Bomb";
+                    }
+                    else
+                    {
+                        arrButtons[i, j].Tag = "not bomb";
+                    }
+
+                    arrButtons[i, j].MouseWheel += new MouseEventHandler(this.arrButtons_wheelClicked);//Flag
+                    arrButtons[i, j].MouseClick += new MouseEventHandler(this.arrButtons_click);//Click
+                }
+            }
+            lblNbrOfBombs.Text = "Number of bombs : " + Convert.ToString(nbrOfBomb);
+        }
+
+        /// <summary>
+        /// Method to check if the button that was clicked is not a Bomb and to know how far the bomb is from the clicked button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void arrButtons_click(object sender, MouseEventArgs e)
+        {
+            Button btnClicked = (Button)sender;
+            if (btnClicked.Tag == "Bomb")
+            {
+                btnClicked.Text = "Was a bomb";
+                for (int i = 0; i < NBROWCOLS; i++)
+                {
+                    for (int j = 0; j < NBROWCOLS; j++)
+                    {
+                        if (arrButtons[i,j].Tag == "Bomb")
+                        {
+                            arrButtons[i, j].BackColor = Color.Red;
+                        }
+                    }
+                }
+                DialogResult result = MessageBox.Show("Perdu rejouer ?", "Partie terminée", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+                if (result == DialogResult.Yes)
+                {
+                    Application.Restart();
+                }
+                else
+                {
+                    Close();
+                }
+            }
+            else
+            {
+                score++;
+                lblScore.Text = "Score : " + score;
+                btnClicked.Enabled = false;
+                quit = false;
+
+                if (score == 100-nbrOfBomb)//Won
+                {
+                    DialogResult win = MessageBox.Show("Gagné ! Rejouer ?", "Partie terminée", MessageBoxButtons.YesNo, MessageBoxIcon.Stop);
+                    if (win == DialogResult.Yes)
+                    {
+                        Application.Restart();
+                    }
+                    else
+                    {
+                        Close();
+                    }
+                }
+
+                //One away from bomb
+                for (int i = 0; i < NBROWCOLS; i++)
+                {
+                    for (int j = 0; j < NBROWCOLS; j++)
+                    {
+                        if (arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.X + 94 == arrButtons[i, j].Location.X && btnClicked.Location.Y == arrButtons[i, j].Location.Y || arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.X - 94 == arrButtons[i, j].Location.X && btnClicked.Location.Y == arrButtons[i, j].Location.Y || arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.Y + 38 == arrButtons[i, j].Location.Y && btnClicked.Location.X == arrButtons[i, j].Location.X || arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.Y - 38 == arrButtons[i, j].Location.Y && btnClicked.Location.X == arrButtons[i, j].Location.X)
+                        {
+                            btnClicked.Text = "1";
+                            quit = true;
+                            break;
+                        }
+                    }
+                    if (quit == true)
+                    {
+                        break;
+                    }
+                }
+                
+                //Two away from bomb
+                if (quit == false)
+                {
+                    for (int i = 0; i < NBROWCOLS; i++)
+                    {
+                        for (int j = 0; j < NBROWCOLS; j++)
+                        {
+                            if (arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.X + 188 == arrButtons[i, j].Location.X && btnClicked.Location.Y == arrButtons[i, j].Location.Y || arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.X - 188 == arrButtons[i, j].Location.X && btnClicked.Location.Y == arrButtons[i, j].Location.Y || arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.Y + 76 == arrButtons[i, j].Location.Y && btnClicked.Location.X == arrButtons[i, j].Location.X || arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.Y - 76 == arrButtons[i, j].Location.Y && btnClicked.Location.X == arrButtons[i, j].Location.X)
+                            {
+                                btnClicked.Text = "2";
+                                quit = true;
+                                break;
+                            }
+                        }
+                        if (quit == true)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                //Three away from bomb
+                if (quit == false)
+                {
+                    for (int i = 0; i < NBROWCOLS; i++)
+                    {
+                        for (int j = 0; j < NBROWCOLS; j++)
+                        {
+                            if (arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.X + 282 == arrButtons[i, j].Location.X && btnClicked.Location.Y == arrButtons[i, j].Location.Y || arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.X - 282 == arrButtons[i, j].Location.X && btnClicked.Location.Y == arrButtons[i, j].Location.Y || arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.Y + 114 == arrButtons[i, j].Location.Y && btnClicked.Location.X == arrButtons[i, j].Location.X || arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.Y - 114 == arrButtons[i, j].Location.Y && btnClicked.Location.X == arrButtons[i, j].Location.X)
+                            {
+                                btnClicked.Text = "3";
+                                quit = true;
+                                break;
+                            }
+                        }
+                        if (quit == true)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                //Four away from bomb
+                if (quit == false)
+                {
+                    for (int i = 0; i < NBROWCOLS; i++)
+                    {
+                        for (int j = 0; j < NBROWCOLS; j++)
+                        {
+                            if (arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.X + 376 == arrButtons[i, j].Location.X && btnClicked.Location.Y == arrButtons[i, j].Location.Y || arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.X - 376 == arrButtons[i, j].Location.X && btnClicked.Location.Y == arrButtons[i, j].Location.Y || arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.Y + 152 == arrButtons[i, j].Location.Y && btnClicked.Location.X == arrButtons[i, j].Location.X || arrButtons[i, j].Tag == "Bomb" && btnClicked.Location.Y - 152 == arrButtons[i, j].Location.Y && btnClicked.Location.X == arrButtons[i, j].Location.X)
+                            {
+                                btnClicked.Text = "4";
+                                quit = true;
+                                break;
+                            }
+                        }
+                        if (quit == true)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Flag the button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void arrButtons_wheelClicked(object sender, MouseEventArgs e)
+        {
+            Button mouseWheelButton = (Button)sender;
+
+            if (mouseWheelButton.Image != flag)
+            {
+                mouseWheelButton.Image = flag;
+            }
+            else
+            {
+                mouseWheelButton.Image = null;
+            }
+        }
+
+        /// <summary>
+        /// Restart the game
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
